f0cf12f59828dd8382c108e5e884d58e2f6dadde
Terje Hoås <terjehoaas@gmail.com>
2011-12-31 20:35:16 +0100

    Now output two lines. Also strips newlines and adds ', ' instead.

--- Wolfram/plugin.py
+++ Wolfram/plugin.py
@@ -19,19 +19,28 @@ class Wolfram(callbacks.Privmsg):
         tree = ElementTree.fromstring(xml)
 
         found = False
+	maxoutput = 2
+	output = 0
         for pod in tree.findall('.//pod'):
             title = pod.attrib['title']
             for plaintext in pod.findall('.//plaintext'):
                 if plaintext.text:
                     found = True
-                    if(title == "Input interpretation" or title == "Result" or title == "Input" or title == "Exact result" or title == "Decimal approximation"):
-                        irc.reply(("%s: %s" % (title, plaintext.text)).encode('utf-8'))
+                    """if(title == "Input interpretation" or 
+                    title == "Result" or 
+                    title == "Input" or 
+                    title == "Exact result" or 
+                    title == "Decimal approximation"):
+                    """
+                    if output < maxoutput:
+                        irc.reply(("%s: %s" % (title,
+                            plaintext.text.replace('\n', ', '))).encode('utf-8'))
+                        output += 1
         if not found:
             irc.reply("huh, I dunno, I'm still a baby AI. Wait till the singularity I guess?")
-#        else:
-#            irc.reply(reply.encode('utf-8'))
 
     alpha = wrap(alpha, ['text'])
 
 
 Class = Wolfram
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
