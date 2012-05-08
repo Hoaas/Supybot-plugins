7a3a9713cb5e41ce27d7c162d002b58d79055c03
Terje Hoås <terjehoaas@gmail.com>
2012-05-08 09:33:51 +0200

    Twitter: Added function for escaping/translating HTML characters. Patch from atr000 @ github.

--- Twitter/plugin.py
+++ Twitter/plugin.py
@@ -45,6 +45,27 @@ class Twitter(callbacks.Plugin):
     trending tweets."""
     threaded = True
 
+    def _unescape(self, text):
+      def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except (ValueError, OverflowError):
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+      return re.sub("&#?\w+;", fixup, text)
+
     def twitter(self, irc, msg, args, nick, reply, rt):
         """<nick> [--reply] [--rt]
 
