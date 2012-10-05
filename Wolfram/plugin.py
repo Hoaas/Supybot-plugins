578add085a553bfe3e0cecf7b23b0dc5b35ff78b
Terje Hoås <terjehoaas@gmail.com>
2012-10-05 21:12:32 +0200

    WA: Add number of output option.

--- Wolfram/plugin.py
+++ Wolfram/plugin.py
@@ -6,15 +6,21 @@ from xml.etree import ElementTree
 
 class Wolfram(callbacks.Privmsg):
 
-    def alpha(self, irc, msg, args, question):
-        """Ask Mr. Wolfram a question, get an "answer"...maybe? It uses the
-        Wolfram Alpha API.
+    def alpha(self, irc, msg, args, options, question):
+        """[--lines num] <query>
+
+        Ask Mr. Wolfram a question, get an "answer"...maybe? It uses the Wolfram Alpha API.
         <http://products.wolframalpha.com/docs/WolframAlpha-API-Reference.pdf>
         """
         apikey = self.registryValue('apikey')
         if not apikey or apikey == "Not set":
             irc.reply("API key not set. see 'config help supybot.plugins.Wolfram.apikey'.")
             return
+
+	maxoutput = 2
+        for (key, value) in options:
+            if key == 'lines':
+                maxoutput = value
 
         u = "http://api.wolframalpha.com/v2/query?"
         q = urllib.urlencode({'input': question, 'appid': apikey})
@@ -30,7 +36,6 @@ class Wolfram(callbacks.Privmsg):
             return
 
         found = False
-	maxoutput = 2
 	outputcount = 0
         for pod in tree.findall('.//pod'):
             title = pod.attrib['title']
@@ -47,12 +52,17 @@ class Wolfram(callbacks.Privmsg):
                         output = plaintext.text
                         output = output.replace(' | ', ': ')
                         output = output.replace('\n', ', ')
+                        # Skip the input interpretation if only one line out.
+                        if maxoutput == 1 and outputcount == 0:
+                            maxoutput = 2 # hack :D
+                            outputcount += 1
+                            continue
                         irc.reply(("%s: %s" % (title, output.encode('utf-8'))))
                         outputcount += 1
         if not found:
             irc.reply("huh, I dunno, I'm still a baby AI. Wait till the singularity I guess?")
 
-    alpha = wrap(alpha, ['text'])
+    alpha = wrap(alpha, [getopts({'lines':'int'}), 'text'])
 
 
 Class = Wolfram
