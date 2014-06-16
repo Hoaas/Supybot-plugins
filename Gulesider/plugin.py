126ccb7655f5a3875b6bcead3c349d7e6cede0b1
Terje Hoås <terjehoaas@gmail.com>
2014-06-16 21:12:31 +0200

    Gulesider: Updated as the HTML have changed on the site.

--- Gulesider/plugin.py
+++ Gulesider/plugin.py
@@ -61,126 +61,62 @@ class Gulesider(callbacks.Plugin):
         html = utils.web.getUrl(url)
         return html
 
+    def formataddress(self, p):
+        return ', '.join([_f for _f in (ircutils.bold(p.name), p.address()) if _f])
 
-    def getsinglehit(self, soup):
-        hit = soup.find(class_='profile-main')
-        if hit:
-            return hit, 1
-        return None, None
-
-    def getmultiplehits(self, soup):
-        hits = 0
-        reslist = soup.find(id='result-list')
-        if reslist:
-            reslist = soup.find_all(class_='hit vcard')
-            hits = len(reslist)
-        return reslist, hits
-
-    def fornavn(self, person):
-        fornavn = person.find_all(class_='given-name')
-        if not fornavn:
-            return None
-        elif len(fornavn) != 1:
-            raise Exception('For mange fornavn!')
-        return fornavn[0].text.strip()
-
-    def mellomnavn(self, person):
-        mellomnavn = person.find_all(class_='middleName')
-        if not mellomnavn:
-            return None
-        elif len(mellomnavn) != 1:
-            raise Exception('For mange mellomnavn! Evnt. for mange tags for mellomnavn på gulesider.no')
-        return mellomnavn[0].text.strip()
-
-    def etternavn(self, person):
-        etternavn = person.find_all(class_='family-name')
-        if not etternavn:
-            return None
-        elif len(etternavn) != 1:
-            raise Exception('For mange etternavn!')
-        return etternavn[0].text.strip()
-
-    def getnavn(self, person):
-        fornavn = self.fornavn(person)
-        mellomnavn = self.mellomnavn(person)
-        etternavn = self.etternavn(person)
-        navn = ''
-        if fornavn:
-            navn += fornavn + ' '
-        if mellomnavn:
-            navn += mellomnavn + ' '
-        if etternavn:
-            navn += etternavn
-        if navn == '':
-            navn = None
-        return navn.strip()
-
-    def gettlf(self, person):
-        tlf = person.find_all(class_='value')
-        if not tlf:
-            return None
-        elif len(tlf) > 1:
-            ret = []
-            for t in tlf:
-                ret.append(t.text.strip())
-            return ' / '.join(ret)
-        return tlf[0].text.strip()
-
-    def getadresse(self, person):
-        gate = person.find_all(class_='street-address')
-        if gate and len(gate) > 0:
-            gate = gate[0].text.strip()
-        postnr = person.find_all(class_='postal-code')
-        if postnr and len(postnr) > 0:
-            postnr = postnr[0].text.strip()
-        by = person.find_all(class_='locality')
-        if by and len(by) > 0:
-            by = by[0].text.strip()
-        by = ' '.join([_f for _f in (postnr, by) if _f])
-        return ', '.join([_f for _f in (gate, by) if _f])
-
-    def formatperson(self, p, num):
-        navn = self.getnavn(p)
-        adresse = self.getadresse(p)
-        tlf = ircutils.bold(self.gettlf(p))
-        formatedperson = ''
-        if num:
-            formatedperson = ', '.join([_f for _f in (ircutils.bold(navn), adresse) if _f])
-        else:
-            navn_og_adresse = ', '.join([_f for _f in (navn, adresse) if _f])
-            formatedperson = ' - '.join([_f for _f in (tlf, navn_og_adresse) if _f])
-        return formatedperson
-
+    def parsepersons(self, soup):
+        persons = []
+        hits = soup.find_all(class_='hit-header-block-center')
+        for hit in hits:
+            name = tlf = zipcode = location = street = None
+            namefield = hit.find(class_='hit-name')
+            if namefield:
+                name = namefield.find('a').text.replace('\n', '')
+            tlffield = hit.find(class_='hit-address')
+            if tlffield:
+                tlffield2 = tlffield.find(class_='hit-phone-number')
+                if tlffield2:
+                    tlf = tlffield2.text
+            streetfield = hit.find(class_='hit-street-address')
+            if streetfield:
+                street = streetfield.text.replace('\n', '')
+            zipcodefield = hit.find(class_='hit-postal-code')
+            if zipcodefield:
+                zipcode = zipcodefield.text
+            locationfield = hit.find(class_='hit-address-locality')
+            if locationfield:
+                location = locationfield.text
+            if not name and not tlf and not zipcode and not location and not street:
+                continue
+            p = Person(name, tlf, zipcode, location, street)
+            persons.append(p)
+        return persons
 
     def tlf(self, irc, msg, args, text):
         """<name | number>
 
-        Henter informasjon fra gulesider.no"""
+        Henter informasjon fra gulesider.no. Sjekker også en lokal kopi av telefonterror.no."""
+        isdigit = self.isdigit(text)
+        if isdigit:
+            terror = self.tlfterror(text)
+            if terror:
+                irc.reply(ircutils.bold(terror) + ', ifølge telefonterror.no')
+                return
 
-        num = self.isdigit(text)
         html = self.fetch(text)
         soup = BS(html)
 
-        hits = 0
-        persons, hits = self.getsinglehit(soup)
-        if not persons:
-            persons, hits = self.getmultiplehits(soup)
-
-        if not persons or len(persons) == 0:
-            if num:
-                terror = self.tlfterror(text)
-                if terror:
-                    irc.reply(ircutils.bold(terror) + ', ifølge telefonterror.no')
-                    return
+        persons = self.parsepersons(soup);
+        if len(persons) == 0:
             irc.reply('No hits.')
             return
-
-        ret = ''
-        if hits == 1:
-            ret = self.formatperson(persons, num)
-        else:
-            for p in persons:
-                ret = '; '.join([_f for _f in (ret, self.formatperson(p, num)) if _f])
+        lines = []
+        for p in persons:
+            if isdigit:
+                lines.append(self.formataddress(p))
+            else:
+                lines.append(p.tlf + ' - ' + self.formataddress(p))
+        ret = '; '.join([_f for _f in (l for l in lines) if _f])
         irc.reply(ret)
     tlf = wrap(tlf, ['text'])
 
@@ -203,8 +139,16 @@ class Gulesider(callbacks.Plugin):
             if num_and_name[0] == num:
                 return num_and_name[1].strip()
 
-
 Class = Gulesider
 
-
-# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
+class Person:
+    def __init__(self, name, tlf, zipcode, location, street):
+        self.name = name
+        self.tlf = tlf
+        self.zipcode = zipcode
+        self.location = location
+        self.street = street
+    def address(self):
+        loc = ' '.join([_f for _f in (self.zipcode, self.location) if _f])
+        loc = ', '.join([_f for _f in (self.street, loc) if _f])
+        return loc
