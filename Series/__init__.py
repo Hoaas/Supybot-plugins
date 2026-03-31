7b7edbf0064c7caf8e5f09da947ecfbf66e23a07
Terje Hoås <terje@robogoat.dev>
2026-03-31 22:53:33 +0200

    Series: Updated. Removed usage of episodeworld and tvrage.

--- Series/__init__.py
+++ Series/__init__.py
@@ -1,4 +1,3 @@
-# coding=utf8
 ###
 # Copyright (c) 2010, Terje Hoås
 # All rights reserved.
@@ -30,40 +29,26 @@
 ###
 
 """
-Add a description of the plugin (to be presented to the user inside the wizard)
-here.  This should describe *what* the plugin does.
+Returns TV show information and episode data via the TVmaze API.
 """
 
 import supybot
 import supybot.world as world
 
-
-# Use this for the version of this plugin.  You may wish to put a CVS keyword
-# in here if you're keeping the plugin in CVS or some similar system.
 __version__ = ""
 
-# XXX Replace this with an appropriate author or supybot.Author instance.
-__author__ = supybot.Author('Terje Hoås', 'Hoaas', 'terjehoaas@gmail.com')
+__author__ = supybot.Author('Terje Hoås', 'Hoaas', 'terje@robogoat.dev')
 
-# This is a dictionary mapping supybot.Author instances to lists of
-# contributions.
 __contributors__ = {}
-
-# This is a url where the most recent plugin package can be downloaded.
-__url__ = '' # 'http://supybot.com/Members/yourname/Series/download'
 
 from . import config
 from . import plugin
 from importlib import reload
-reload(plugin) # In case we're being reloaded.
-# Add more reloads here if you add third-party modules and want them to be
-# reloaded when this plugin is reloaded.  Don't forget to import them as well!
+reload(config)
+reload(plugin)
 
 if world.testing:
     from . import test
 
 Class = plugin.Class
 configure = config.configure
-
-
-# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
--- Series/config.py
+++ Series/config.py
@@ -1,4 +1,3 @@
-# coding=utf8
 ###
 # Copyright (c) 2010, Terje Hoås
 # All rights reserved.
@@ -32,19 +31,16 @@
 import supybot.conf as conf
 import supybot.registry as registry
 
+try:
+    from supybot.i18n import PluginInternationalization
+    _ = PluginInternationalization('Series')
+except ImportError:
+    _ = lambda x: x
+
+
 def configure(advanced):
-    # This will be called by supybot to configure this module.  advanced is
-    # a bool that specifies whether the user identified himself as an advanced
-    # user or not.  You should effect your configuration by manipulating the
-    # registry as appropriate.
     from supybot.questions import expect, anything, something, yn
     conf.registerPlugin('Series', True)
 
 
 Series = conf.registerPlugin('Series')
-# This is where your configuration variables (if any) should go.  For example:
-# conf.registerGlobalValue(Series, 'someConfigVariableName',
-#     registry.Boolean(False, """Help for someConfigVariableName."""))
-
-
-# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
