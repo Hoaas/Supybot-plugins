4cbb4dac4dbcfe05d73345a4e5e462c91fc54f8e
Terje Hoås <terje@robogoat.dev>
2026-04-01 18:32:58 +0200

    Removed old comments

--- OMDb/__init__.py
+++ OMDb/__init__.py
@@ -35,28 +35,16 @@ OMDb: Information about movies (and maybe series?) from omdbapi.com, which, at t
 import supybot
 import supybot.world as world
 
-# Use this for the version of this plugin.  You may wish to put a CVS keyword
-# in here if you're keeping the plugin in CVS or some similar system.
 __version__ = ""
-
-# XXX Replace this with an appropriate author or supybot.Author instance.
 __author__ = supybot.Author('Terje Hoås', 'Hoaas', 'terje@robogoat.dev')
-
-# This is a dictionary mapping supybot.Author instances to lists of
-# contributions.
 __contributors__ = {}
-
-# This is a url where the most recent plugin package can be downloaded.
 __url__ = ''
 
 from . import config
 from . import plugin
 from importlib import reload
-# In case we're being reloaded.
 reload(config)
 reload(plugin)
-# Add more reloads here if you add third-party modules and want them to be
-# reloaded when this plugin is reloaded.  Don't forget to import them as well!
 
 if world.testing:
     from . import test
