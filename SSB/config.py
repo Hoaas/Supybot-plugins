fb3792d30b34066ed78f6d437c82eb2a6a6f9d17
Terje Hoås <terje@robogoat.dev>
2026-04-01 19:33:46 +0200

    SSB: Updated

--- SSB/config.py
+++ SSB/config.py
@@ -1,4 +1,3 @@
-# coding=utf8
 ###
 # Copyright (c) 2012, Terje Hoås
 # All rights reserved.
@@ -31,23 +30,20 @@
 
 import supybot.conf as conf
 import supybot.registry as registry
-from supybot.i18n import PluginInternationalization, internationalizeDocstring
+try:
+    from supybot.i18n import PluginInternationalization
+    _ = PluginInternationalization('SSB')
+except ImportError:
+    _ = lambda x: x
 
-_ = PluginInternationalization('SSB')
 
 def configure(advanced):
-    # This will be called by supybot to configure this module.  advanced is
-    # a bool that specifies whether the user identified himself as an advanced
-    # user or not.  You should effect your configuration by manipulating the
-    # registry as appropriate.
     from supybot.questions import expect, anything, something, yn
     conf.registerPlugin('SSB', True)
 
 
 SSB = conf.registerPlugin('SSB')
-# This is where your configuration variables (if any) should go.  For example:
-# conf.registerGlobalValue(SSB, 'someConfigVariableName',
-#     registry.Boolean(False, _("""Help for someConfigVariableName.""")))
-
-
-# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
+conf.registerGlobalValue(SSB, 'language', registry.String('', """Override
+    the language for this plugin. Leave empty to use the global
+    supybot.language setting. Accepts any locale code with a matching .po
+    file, including 'no' (Norwegian)."""))
